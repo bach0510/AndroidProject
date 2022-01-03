e17674fc62d61a6f1ed168a9d3b5bcebb316f4f2
bach0510 <bachbc0510@gmail.com>
2022-01-03 22:21:57 +0700

    FIx Input and freezing screen , add search event Bachnx 20210301

--- app/src/main/java/edu/xda/petstore/Home.java
+++ app/src/main/java/edu/xda/petstore/Home.java
@@ -5,18 +5,31 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Home extends AppCompatActivity {
 
+    ImageButton searchIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
+        searchIcon = findViewById(R.id.searchIcon);
+        searchIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(Home.this,"Đã Search", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
