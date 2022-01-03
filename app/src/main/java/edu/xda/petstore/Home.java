c3597acb19846b49e29d674442e7a9f24c1d7739
bach0510 <bachbc0510@gmail.com>
2022-01-03 14:30:08 +0700

    create

---
+++ app/src/main/java/edu/xda/petstore/Home.java
@@ -0,0 +1,22 @@
+package edu.xda.petstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
