5b7b3d13efc0c1752e0fcf9a91ef5b83e1fdfb21
bach0510 <bachbc0510@gmail.com>
2022-01-07 19:56:25 +0700

    Change Start Intent to Login (note: khong can nhap text , chi can nhan login) Bachnx 20210107

--- app/src/main/java/edu/xda/petstore/Login.java
+++ app/src/main/java/edu/xda/petstore/Login.java
@@ -2,13 +2,28 @@ package edu.xda.petstore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login.this, Home.class);
+                Login.this.startActivity(i);
+            }
+        });
+
     }
 }
